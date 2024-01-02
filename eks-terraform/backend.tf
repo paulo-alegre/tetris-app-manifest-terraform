1143114c29b35e75cbfae562d942a6ced76f731b
paulo <pauloalegre0207@gmail.com>
2024-01-02 11:00:22 +0800

    created another tf state for eks

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-12" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "EKS/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
